cea79f88699a07d6deaee3f6592113958c7852aa
anstice <ko.an.2018@yandex.ru>
2023-02-23 20:50:44 +0300

    task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -2,6 +2,7 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+//1. Выведение координат
 double x1, y1, z1;
 double x2, y2, z2;
 
@@ -21,7 +22,7 @@ y2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("z2: ");
 z2 = Convert.ToDouble(Console.ReadLine());
 
-
+//2. Метод для вывода расстояния
 double Distance (double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double dist = Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0), 0.5);
